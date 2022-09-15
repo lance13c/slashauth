78656827f615b05da6bcfdd99cb1570bb8672839
Dominic Cicilio <dominic@yoamigo.io>
2022-09-14 23:21:33 -0400

    add baseUrl to typescript

--- app/ui/Ticket/index.tsx
+++ app/ui/Ticket/index.tsx
@@ -6,7 +6,7 @@ interface TicketProps extends MongooseTicket {}
 
 const Ticket: React.FunctionComponent<TicketProps> = ({ _id, title, description, status, assignee }) => {
   return (
-    <div className={styles.title}>
+    <div key={_id} className={styles.title}>
       <TicketTitle title={title} />
       <p>{description}</p>
       <p>{status}</p>
