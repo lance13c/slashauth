8b48dd48be96cf32c9f95e42c591c099fc4b20a4
Dominic Cicilio <dominic@yoamigo.io>
2022-09-16 21:27:24 -0400

    update form styles, makes description a textbox

--- app/ui/TicketForm/index.tsx
+++ app/ui/TicketForm/index.tsx
@@ -71,64 +71,71 @@ const TicketForm: React.FunctionComponent<TicketFormProps> = ({ onComplete }) =>
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)} className={styles.ticketForm}>
       {/* register your input into the hook by invoking the "register" function */}
-      <List
+      <div
         style={{
-          flexDirection: 'column',
-          width: 'initial',
-          padding: 0,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '1.5rem',
         }}
       >
-        <label htmlFor='title'>Title</label>
-        <input name='title' autoFocus defaultValue='' required {...register('title', { required: true })} />
-        {errors?.title?.message && <ErrorMessage>{errors.title.message}</ErrorMessage>}
-      </List>
-      <List
-        style={{
-          flexDirection: 'column',
-          width: 'initial',
-          padding: 0,
-        }}
-      >
-        <label htmlFor='description'>Description</label>
-        <input name='description' defaultValue='' {...register('description')} />
-        {errors?.description?.message && <p>{errors.description.message}</p>}
-      </List>
-      <List
-        style={{
-          flexDirection: 'column',
-          width: 'initial',
-          padding: 0,
-        }}
-      >
-        <label htmlFor='status'>Status</label>
-        <select name='status' {...register('status')} defaultValue='BACKLOG'>
-          <option value='BACKLOG'>BACKLOG</option>
-          <option value='IN_PROGRESS'>IN_PROGRESS</option>
-          <option value='COMPLETED'>COMPLETED</option>
-          <option value='BLOCKED'>BLOCKED</option>
-        </select>
-        {errors?.status?.message && <p>{errors.status.message}</p>}
-      </List>
-      <List
-        style={{
-          flexDirection: 'column',
-          width: 'initial',
-          padding: 0,
-        }}
-      >
-        <label htmlFor='assigneeId'>Assignee</label>
-        {errors?.assigneeId?.message && <p>{errors.assigneeId.message}</p>}
-        <select name='assigneeId' {...register('assigneeId')}>
-          {users.map((user) => {
-            return (
-              <option key={user?._id} value={user.name}>
-                {user.name}
-              </option>
-            );
-          })}
-        </select>
-      </List>
-
+        <List
+          style={{
+            flexDirection: 'column',
+            width: 'initial',
+            padding: 0,
+          }}
+        >
+          <label htmlFor='title'>Title</label>
+          <input name='title' autoFocus defaultValue='' required {...register('title', { required: true })} />
+          {errors?.title?.message && <ErrorMessage>{errors.title.message}</ErrorMessage>}
+        </List>
+        <List
+          style={{
+            flexDirection: 'column',
+            width: 'initial',
+            padding: 0,
+          }}
+        >
+          <label htmlFor='description'>Description</label>
+          <textarea name='description' defaultValue='' rows={2} {...register('description')} />
+          {errors?.description?.message && <p>{errors.description.message}</p>}
+        </List>
+        <List
+          style={{
+            flexDirection: 'column',
+            width: 'initial',
+            padding: 0,
+          }}
+        >
+          <label htmlFor='status'>Status</label>
+          <select name='status' {...register('status')} defaultValue='BACKLOG'>
+            <option value='BACKLOG'>BACKLOG</option>
+            <option value='IN_PROGRESS'>IN_PROGRESS</option>
+            <option value='COMPLETED'>COMPLETED</option>
+            <option value='BLOCKED'>BLOCKED</option>
+          </select>
+          {errors?.status?.message && <p>{errors.status.message}</p>}
+        </List>
+        <List
+          style={{
+            flexDirection: 'column',
+            width: 'initial',
+            padding: 0,
+          }}
+        >
+          <label htmlFor='assigneeId'>Assignee</label>
+          <select name='assigneeId' {...register('assigneeId')}>
+            {users.map((user) => {
+              return (
+                <option key={user?._id} value={user.name}>
+                  {user.name}
+                </option>
+              );
+            })}
+          </select>
+          {errors?.assigneeId?.message && <p>{errors.assigneeId.message}</p>}
+        </List>
+      </div>
       <input type='submit' />
     </form>
   );
