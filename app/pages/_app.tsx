c6adce27c667d0bb18abaa29f6b4e21813c7c22e
Dominic Cicilio <dominic@yoamigo.io>
2022-09-18 20:36:59 -0400

    removes root eslint and typescript, adds root configs to app

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
         <style>@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');</style>
       </Head>
 
